68892fe7af4ac1ffd0ef9c1d7215b66955762057
Nkono Ndeme Miguel <nkonondememiguel7@gmail.com>
2025-10-16 07:04:01 +0100

    fix a small thing in the code base

--- syntax_highlighting/syntax_highlighting.h
+++ syntax_highlighting/syntax_highlighting.h
@@ -9,11 +9,13 @@
 
 /**
  * Function to highlight each character of a row.
+ * @param row which is the row of text in the editor.
  * */
 void editorUpdateSyntax(erow *row);
 
 /**
- * Function to map the value in hl std::array of a row to actual ANSI color code.
+ * Function to map the value in hl array of a row to actual ANSI color code.
+ * @param hl which is the color.
  * */
 int editorSyntaxToColor(int hl);
 
@@ -23,6 +25,9 @@ int editorSyntaxToColor(int hl);
  *   - space
  *   - '\0'
  *   - ,.[](){}+-/%~<>;
+ *
+ * @param ch which is the character to check agains.
+ * @return [int] will return 1 indicating that the character is a separator otherwise it will return an other digit.
  **/
 int is_separator(int ch);
 
